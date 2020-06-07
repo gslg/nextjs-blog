7fcc40f5c7f37f9948df379fd9e007b0a0595d92
lg <sclg1992@163.com>
2020-06-07 18:18:11 +0800

    1.主页样式修改， 2.支持多级路由 3.支持markdown本地图片url转路由图片 4.写一篇nginx博客 5.引入antdesign

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 
-const name = 'Nicolas'
+const name = 'August'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import TimelimeLabelDemo from '../components/timeline'
+
 
 export default function Home({ allPostsData }) {
   return (
@@ -12,28 +12,15 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[生命诚可贵,爱情价更高,若为自由故,两者皆可抛]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (使用<a href="https://nextjs.org/">Next.js</a>搭建的个人博客,.)
         </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <TimelimeLabelDemo blogs={allPostsData} />
       </section>
     </Layout>
   )
